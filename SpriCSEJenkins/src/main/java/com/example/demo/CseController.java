768be2325bcefd9726d23f8936c1a71cb727a8f3
ramya <ramyathumati456@gmail.com.in>
2025-09-13 15:53:21 +0530

    Hema first commit

--- SpriCSEJenkins/src/main/java/com/example/demo/CseController.java
+++ SpriCSEJenkins/src/main/java/com/example/demo/CseController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CseController {
 	@GetMapping("/home")
 	public String rocks() {
-		return "welcome";
+		return "welcome+dp";
 		
 	}
 
